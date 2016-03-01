8ea9370a806883e1b2619684620b35b2dc8cdc89
Mario Galindo <marioga_lindo@hotmail.es>
2016-03-01 02:09:40 -0600

    Commit Inicial

---
+++ AngularMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetProducts() {
+
+            var db = new ProductosDBEntities();
+            var products = db.Productos.ToList();
+
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AngularMVC/Scripts/App.js
@@ -0,0 +1,13 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function ($scope, $http) {
+
+    $http.get('/home/GetProducts')
+        .success(function (result) {
+            $scope.products = result;
+        })
+        .error(function (data) {
+            console.log(data);
+        })
+
+});
